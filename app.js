00e9bb6e59343aaac35ae08df4a67376bdb09b85
thaypcosta <117128583+thaypcosta@users.noreply.github.com>
2024-12-20 14:50:09 -0300

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 alert('Boas-vindas ao Jogo do Número Secreto');
-let numeroSecreto = 10;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
-let  chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
 
+// enquanto chute não for igual ao n.s.
+while (chute!= numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
 // se o chute for igual ao número secreto
-if (numeroSecreto == chute) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+    if (numeroSecreto == chute) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas}`);
+    } else {
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++
+    }
 }
